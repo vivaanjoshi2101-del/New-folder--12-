b6603ce151e99515af411a9374c7ad225ccbcb21
vivaanjoshi2101-del <vivaanjoshi2101@gmail.com>
2026-09-05 18:32:28 +0530

    initial commit

---
+++ functionCalculator.py
@@ -0,0 +1,35 @@
+print("Choose any of the following options:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide\n")
+
+try:
+    func = int(input("Enter your choice (1,2,3 or 4): "))
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+except ValueError:
+    print("Invalid input. Please enter valid numbers.")
+
+
+def add(num1,num2):
+    return num1 + num2
+
+def subtract(num1,num2):
+    return num1 - num2
+
+def multiply(num1,num2):
+    return num1 * num2
+
+def divide(num1,num2):
+    return num1 / num2
+
+
+if func == 1:
+    print(add(num1, num2))
+elif func == 2:
+    print(subtract(num1, num2))
+elif func == 3:
+    print(multiply(num1, num2))
+elif func == 4:
+    print(divide(num1, num2))
